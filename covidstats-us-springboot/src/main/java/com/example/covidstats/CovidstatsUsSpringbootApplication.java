b9fdcd119c82add3828247f4bc3cf720121ddeba
yateeshhegde <yateeshhegde@localhost>
2022-07-11 12:23:19 +0530

    spring boot init

---
+++ covidstats-us-springboot/src/main/java/com/example/covidstats/CovidstatsUsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.covidstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidstatsUsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidstatsUsSpringbootApplication.class, args);
+	}
+
+}
